fb32aca87650e2519579fc74a8e245a13cffe1ab
Tony <antonio.augusto.castanheira@gmail.com>
2023-06-13 18:52:36 -0300

    Removing more Golang stuff

--- home/antoniocastanheira/ide/files/astronvim/plugins/mason.lua
+++ home/antoniocastanheira/ide/files/astronvim/plugins/mason.lua
@@ -13,7 +13,7 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      ensure_installed = { "iferr", "impl", "goimports" },
+      ensure_installed = { "iferr", "impl" },
     },
   },
   {
